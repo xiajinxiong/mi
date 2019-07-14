38cba27ae99781804d866c0b05ff528da8e469f5
xiajinxiong <529783166@qq.com>
2019-07-14 19:48:58 +0800

    加入sql

---
+++ sqlsql.sql
@@ -0,0 +1,222 @@
+﻿-- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: mi
+-- ------------------------------------------------------
+-- Server version	8.0.15
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pword` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admin`
+--
+
+LOCK TABLES `admin` WRITE;
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cart`
+--
+
+DROP TABLE IF EXISTS `cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `cart` (
+  `user_id` int(20) NOT NULL,
+  `product_id` int(20) NOT NULL,
+  `version_id` int(20) NOT NULL,
+  `number` int(11) DEFAULT NULL,
+  PRIMARY KEY (`user_id`,`product_id`,`version_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cart`
+--
+
+LOCK TABLES `cart` WRITE;
+/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
+INSERT INTO `cart` VALUES (1,1,1,1),(1,1,2,1),(9,1,1,2),(9,1,2,1);
+/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `p_order`
+--
+
+DROP TABLE IF EXISTS `p_order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `p_order` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `version_id` int(20) NOT NULL,
+  `user_id` int(20) NOT NULL,
+  `number` int(11) NOT NULL,
+  `state` int(11) NOT NULL,
+  `createdAt` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `p_order`
+--
+
+LOCK TABLES `p_order` WRITE;
+/*!40000 ALTER TABLE `p_order` DISABLE KEYS */;
+INSERT INTO `p_order` VALUES (1,1,1,2,0,'2019-07-11 07:50:48'),(2,1,1,1,0,'2019-07-11 17:31:37'),(3,1,1,1,0,'2019-07-11 17:31:41'),(4,1,1,3,0,'2019-07-11 18:31:12'),(5,2,1,1,0,'2019-07-11 18:31:12'),(6,1,1,4,0,'2019-07-11 18:39:29'),(7,2,1,2,0,'2019-07-11 18:39:29'),(8,1,1,4,0,'2019-07-11 18:39:38'),(9,2,1,2,0,'2019-07-11 18:39:38'),(10,1,9,2,0,'2019-07-11 18:42:09'),(11,2,9,1,0,'2019-07-11 18:42:09'),(12,1,1,1,0,'2019-07-11 19:19:34'),(13,2,1,1,0,'2019-07-12 03:41:48');
+/*!40000 ALTER TABLE `p_order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `detail` varchar(255) NOT NULL,
+  `overview` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,'小米6','变焦双摄，4 轴防抖 / 骁龙835 旗舰处理器，6GB 大内存，最大可选128GB 闪存 / 5.15 护眼屏 / 四曲面玻璃/陶瓷机身','5.16早10点开售');
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `taco`
+--
+
+DROP TABLE IF EXISTS `taco`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `taco` (
+  `id` int(11) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `taco`
+--
+
+LOCK TABLES `taco` WRITE;
+/*!40000 ALTER TABLE `taco` DISABLE KEYS */;
+INSERT INTO `taco` VALUES (1,'cat');
+/*!40000 ALTER TABLE `taco` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `nick_name` varchar(45) DEFAULT NULL,
+  `phone` varchar(45) DEFAULT NULL,
+  `pword` varchar(45) NOT NULL,
+  `signature` varchar(255) DEFAULT NULL,
+  `hobby` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `phone_UNIQUE` (`phone`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'xiajx','asdd','sdsdd','12345','111','','sds'),(2,'xiajinxiong',NULL,'11111111111','123',NULL,NULL,NULL),(3,'xiajinxiong1',NULL,'11111111112','123',NULL,NULL,NULL),(4,'xiajinxiong2',NULL,'11111111113','123',NULL,NULL,NULL),(5,'xiajinxiong5',NULL,'11122233333','123',NULL,NULL,NULL),(6,'xiajinxiong7',NULL,'13123344353','123',NULL,NULL,NULL),(7,'xiajinxiong11',NULL,'12312312312','123',NULL,NULL,NULL),(8,'sadsdsd',NULL,'11122222222','123456',NULL,NULL,NULL),(9,'wyc666',NULL,'14484950709','8125799',NULL,NULL,NULL),(10,'xiajinxi',NULL,'18897667890','123',NULL,NULL,NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `version`
+--
+
+DROP TABLE IF EXISTS `version`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `version` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `product_id` int(20) NOT NULL,
+  `version` varchar(255) NOT NULL,
+  `color` varchar(45) NOT NULL,
+  `price` decimal(6,2) NOT NULL,
+  `photo` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`,`product_id`,`version`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `version`
+--
+
+LOCK TABLES `version` WRITE;
+/*!40000 ALTER TABLE `version` DISABLE KEYS */;
+INSERT INTO `version` VALUES (1,1,'全网通版 6GB+128GB','亮黑色',2899.00,'liebiao_xiaomi6.jpg'),(2,1,'全网通版 6GB+64GB','亮黑色',2499.00,'liebiao_xiaomi6.jpg');
+/*!40000 ALTER TABLE `version` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'mi'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-07-14 17:59:39
